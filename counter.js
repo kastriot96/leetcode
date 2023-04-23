5197ae7e2cb9c98ae72a263018445175759d6283
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-04-23 19:46:57 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -3,10 +3,18 @@
  * @return {Function} counter
  */
 
- const createCounter = (n) =>  {
-    let output = [];
-    output = Array(3).fill().map((_, index) => index + n);
-    console.log(output);
-  };
+//  const createCounter = (n) =>  {
+//     let output = [];
+//     output = Array(3).fill().map((_, index) => index + n);
+//     console.log(output);
+//   };
   
-  createCounter(10);
+//   createCounter(10);
+
+ const createCounter = (n) => {
+    let num = n - 1;
+    return () => {
+        num++
+        return num;
+    };
+};
