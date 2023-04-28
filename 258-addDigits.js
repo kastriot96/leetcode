3f17c2138794abf863c43c7d4e58fa3c4f2adbe3
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-04-28 21:49:48 +0200

    Update 258-addDigits.js

--- 258-addDigits.js
+++ 258-addDigits.js
@@ -10,14 +10,20 @@ Given an integer num, repeatedly add all its digits until the result has only on
  */
  var addDigits = function(num) {
     let arrayOfDigits = Array.from(String(num), Number);
+    console.log('arrayOfDigits', arrayOfDigits);
     let sumOfDigits = arrayOfDigits.reduce((total, item) => total + item)
+    console.log('sumOfDigits', sumOfDigits);
     if (sumOfDigits > 9 || arrayOfDigits > 9) {
         arrayOfDigits = arrayOfDigits.reduce((total, item) => total + item)
         arrayOfDigits = Array.from(String(arrayOfDigits), Number);
         sumOfDigits = arrayOfDigits.reduce((total, item) => total + item);
+        if (sumOfDigits > 9 || arrayOfDigits > 9) {
+            arrayOfDigits = arrayOfDigits.reduce((total, item) => total + item)
+            arrayOfDigits = Array.from(String(arrayOfDigits), Number);
+            sumOfDigits = arrayOfDigits.reduce((total, item) => total + item);
+            return sumOfDigits;
+        }
         return sumOfDigits;
-    } else if (sumOfDigits === 10) {
-        return  1;
     }
    return sumOfDigits;
 };
@@ -25,5 +31,4 @@ console.log(addDigits(123212));
 console.log(0);
 console.log(addDigits(19));
 console.log(addDigits(199));
-
-//first idea , still not working for cases 199 , 1999 ... 
+console.log(addDigits(1999));
