b6d56a06f367d6f19f6537b9c894a07197d83e00
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-04 16:24:01 +0200

    Create 3-longestSubstring.js

---
+++ 3-longestSubstring.js
@@ -0,0 +1,15 @@
+/* 
+
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+    
+};
