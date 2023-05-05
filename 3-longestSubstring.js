e894109ff9c5b0efcf6d2b0ba8bda9381fc9b2c4
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-05 15:22:00 +0200

    Update 3-longestSubstring.js

--- 3-longestSubstring.js
+++ 3-longestSubstring.js
@@ -11,7 +11,15 @@ substring
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-    
+     // the idea is to compare each ascii value , and find the longest consecutive ascii 
+     // from index start to index end then convert again ascii to char and we get the longest substring
+    let mapString = Array.from(s);
+    console.log(mapString);
+    let asciiArray = [];
+    for (let i = 0; i < s.length; i++) {
+        asciiArray.push(s.charCodeAt(i));
+      }
+    console.log('asciiArray', asciiArray);
 };
 
-console.log('hi');
+console.log(lengthOfLongestSubstring('jashari'));
