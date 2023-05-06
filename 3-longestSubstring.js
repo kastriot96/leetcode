1324e7e0bbfa39ba171ec13b2180e90c402a106e
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-06 21:01:13 +0200

    Update 3-longestSubstring.js

--- 3-longestSubstring.js
+++ 3-longestSubstring.js
@@ -11,15 +11,21 @@ substring
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-     // the idea is to compare each ascii value , and find the longest consecutive ascii 
-     // from index start to index end then convert again ascii to char and we get the longest substring
+     // the idea is to compare each ascii value , and find the longest consecutive ascii from index start to index end then convert again ascii to char and we get the longest substring
     let mapString = Array.from(s);
     console.log(mapString);
     let asciiArray = [];
+    let longestAscii = [];
     for (let i = 0; i < s.length; i++) {
         asciiArray.push(s.charCodeAt(i));
       }
+    for (let i = 0; i < asciiArray.length; i++) {
+        console.log('el[i]', asciiArray[i]);
+        let currentEl = asciiArray[i];
+        let nextEl = asciiArray[i+1];
+    }
     console.log('asciiArray', asciiArray);
+    console.log('longestAscii', longestAscii);
 };
 
 console.log(lengthOfLongestSubstring('jashari'));
