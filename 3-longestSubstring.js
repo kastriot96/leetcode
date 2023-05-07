9762244b8030fa55c49e4d7634bd672218daedbf
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-07 13:36:00 +0200

    Update 3-longestSubstring.js

--- 3-longestSubstring.js
+++ 3-longestSubstring.js
@@ -1,31 +1,27 @@
-/* 
-
-Given a string s, find the length of the longest 
-substring
- without repeating characters.
-
-*/
-
 /**
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
-     // the idea is to compare each ascii value , and find the longest consecutive ascii from index start to index end then convert again ascii to char and we get the longest substring
-    let mapString = Array.from(s);
-    console.log(mapString);
-    let asciiArray = [];
-    let longestAscii = [];
-    for (let i = 0; i < s.length; i++) {
-        asciiArray.push(s.charCodeAt(i));
-      }
-    for (let i = 0; i < asciiArray.length; i++) {
-        console.log('el[i]', asciiArray[i]);
-        let currentEl = asciiArray[i];
-        let nextEl = asciiArray[i+1];
-    }
-    console.log('asciiArray', asciiArray);
-    console.log('longestAscii', longestAscii);
-};
-
-console.log(lengthOfLongestSubstring('jashari'));
+    
+ let lengthOfLongestSubstring = function (str)
+ {
+     let n = str.length;
+     let res = 0;
+     
+     for(let i = 0; i < n; i++)
+     {
+         let visited = new Array(256);
+         
+         for(let j = i; j < n; j++)
+         {
+             if (visited[str.charCodeAt(j)] == true)
+                 break;
+             else
+             {
+                 res = Math.max(res, j - i + 1);
+                 visited[str.charCodeAt(j)] = true;
+             }
+         }
+     }
+     return res;
+ };
