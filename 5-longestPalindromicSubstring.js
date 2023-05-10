f60d20f911ba3fda9e5a57b08afff91996599f89
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-10 22:29:45 +0200

    Challenge 6

--- 5-longestPalindromicSubstring.js
+++ 5-longestPalindromicSubstring.js
@@ -9,5 +9,29 @@ Given a string s, return the longest palindromic substring in s.
  * @return {string}
 */
 var longestPalindrome = function(s) {
+    let n = s.length;
+    console.log(n);
     
+    /* 
+
+        loop through the string starting from index 0
+        if n = 1 return palindrome as s
+        if n = 2 return there is no palindrome
+        if n > 2 start to check from index[j] === index[n-1] ; j[j+1] === index[n-2]
+    
+    */
+    let res = 0;
+     
+    for(let i = 0; i < n; i++)
+    {
+        let palindrome = []
+        
+        for(let j = i; j < n; j++)
+        {
+         //logic
+        }
+    }
+    return res;
 };
+
+console.log(longestPalindrome('babad'));
