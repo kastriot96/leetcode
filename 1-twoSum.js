ea17f7be44fda526d197df547d74b4e6a5d8ad8b
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-15 21:39:32 +0200

    Update 1-twoSum.js

--- 1-twoSum.js
+++ 1-twoSum.js
@@ -15,5 +15,20 @@ You can return the answer in any order.
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
-};
+      target = 1
+      // To store the required sum
+      var sum = 0;
+      var i, j;
+         
+      // Nested loop for all possible pairs
+      for(i = 0; i <= nums.length - 1; i++)
+      {
+          for(j = i + 1; j <= nums.length - 1; j++)
+          {
+              sum += (nums[i] + nums[j]);
+              console.log('value of i:', nums[i]);
+              console.log('value of j:', nums[j]);
+          }
+      }
+        return Math.max(sum);
+ };
