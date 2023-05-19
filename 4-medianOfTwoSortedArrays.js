0913d8f59201f4bee5eba429ae148db78f928d68
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-19 22:40:05 +0200

    Update 4-medianOfTwoSortedArrays.js

--- 4-medianOfTwoSortedArrays.js
+++ 4-medianOfTwoSortedArrays.js
@@ -11,6 +11,11 @@ The overall run time complexity should be O(log (m+n)).
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-    
+ var findMedianSortedArrays = function(nums1, nums2) {
+    let mergedArray = [...nums1, ...nums2];
+    return mergedArray;
+    //median is the middle number of array when is odd , or the average of two numbers when numbers of elements are even.
+    //check if mergedArray is odd , if is odd return middle num , else if the array is even return average of two middle numbers
 };
+
+console.log(findMedianSortedArrays([1,2],[3]));
