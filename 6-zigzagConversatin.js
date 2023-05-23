e2958ee193ddfe23d0ab184122467773753056bd
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-23 22:36:51 +0200

    Update 6-zigzagConversatin.js

--- 6-zigzagConversatin.js
+++ 6-zigzagConversatin.js
@@ -20,10 +20,21 @@ string convert(string s, int numRows);
  * @return {string}
  */
  var convert = function(s, numRows) {
-     let lengthOfS = s.length;
-    //set the letter with index[numRows + 1] next to letter with index[numRows-1]
-    //set the letter with index[numRows + 2] next to letter with
-    //index[numRows-2]
-};
 
-console.log(convert('PAYPALISHIRING', 4));
+    if(numRows === 1 || s.length < numRows) return s;
+
+    let direction = false;
+    let count = 0;
+
+    let arr = new Array(numRows).fill("");
+
+    for(let i = 0; i < s.length; i++) {
+        let current = s[i];
+
+        arr[count] += current;
+        if(count === 0 || count >= numRows - 1) direction = !direction;
+        direction ? count++ : count--;
+    }
+
+    return arr.join("");
+};
