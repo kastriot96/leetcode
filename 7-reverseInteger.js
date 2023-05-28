e5a8e0196f83176c659cbf19469bc4b8dc29b137
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-05-28 17:07:30 +0200

    Update 7-reverseInteger.js

--- 7-reverseInteger.js
+++ 7-reverseInteger.js
@@ -11,20 +11,19 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
  * @return {number}
  */
  var reverse = function(x) {
-    let myArr = String(x).split("").map((x)=>{
-        return Number(x)
+    let n = Math.abs(x);
+    let myArr = String(n).split("").map((n)=>{
+        return n;
       });
-    let myArrRev = myArr.reverse();
-    console.log(myArrRev);
+    let indexArr = myArr.length - 1;
 
-    if (myArrRev[0] === 0) {
-        return myArrRev.splice(1,myArrRev.length).join("");
+    if (myArr[indexArr] == 0) {
+        let reversedArr = myArr.reverse().splice(1,indexArr).join("");
+        return (x < 0 ? -1 : 1) * reversedArr;
     } else {
-        return myArrRev.join("");
+        let reversedArr = myArr.reverse().join("");
+        return (x < 0 ? -1 : 1) * reversedArr;
     }
-  //add another if block and check if myArrRev[0] === "-" then start splice from 1 and if  myArrRev[0] === "-" && myArrRev[1] === 0 start splice from 2
 };
 
-console.log(reverse(-231));
-
-//need to be checked for negative numbers
+console.log(reverse(-2321));
