f5af79111fa60464ab862f564c3084f39829e58c
Kastriot Jashari <79404857+kastriot96@users.noreply.github.com>
2023-06-15 18:03:23 +0200

    Update 10-regularExpressionMatching.js

--- 10-regularExpressionMatching.js
+++ 10-regularExpressionMatching.js
@@ -13,6 +13,13 @@ The matching should cover the entire input string (not partial).
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
-    
+ var isMatch = function(s, p) {
+    s = '123';
+    p = 'abc';
+    return s + p
+    //case 1 | check if very character of 's' matches with every character of 'p'
+    //case 2 | check if for given characters of 's' and given pattern of 'p' there is aleast 1 character after the pattern char ( meaning exact char what is defined on p)
+    //case 3 | check if for given charaters of 's' there are more one or more chars after the given 'p' condition with (.*)
 };
+
+console.log(isMatch());
